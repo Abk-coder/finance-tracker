b77aa7d8bb8b1648c361a7f10c2d2208ec447041
Abk-coder <dkhadre@ept.sn>
2026-08-18 22:37:25 +0000

    Project setup

--- src/main/java/com/akd/App.java
+++ src/main/java/com/akd/App.java
@@ -2,6 +2,6 @@ package com.akd;
 
 public class App {
     static void main() {
-        IO.println(String.format("Hello, Finance Tracker"));
+        IO.println("Hello, Finance Tracker");
     }
 }
